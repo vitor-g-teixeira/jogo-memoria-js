0ca5b9903bc6dd1e3cbe75817790ded5e68f411b
vitor-g-teixeira <vitorteix20@gmail.com>
2024-11-14 03:22:25 -0300

    Jogo pronto

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Criei, sozinho, um jogo de memória com apenas HTML, CSS e JS.
+
+Fiz para testar meus conhecimentos em JavaScript. Como meu conhecimento na linguagem não é lá muito extenso, deve ter ficado bem ruinzinho. Mas foi uma boa prática.
---
+++ script.js
@@ -0,0 +1,70 @@
+const cardList = [];
+const cardColor = [];
+
+function createGame(){
+    const board = document.querySelector('div#game-board');
+    const cardColorOptions = [
+                                'red', 'red', 
+                                'blue', 'blue', 
+                                'yellow', 'yellow', 
+                                'orange', 'orange',
+                                'brown', 'brown',
+                                'purple', 'purple'
+                            ];
+
+    function selectCardColor(colorOptions){
+        let max = colorOptions.length - 1;
+        const min = 0;
+
+        let randomPosition = Math.floor(Math.random() * (max - min + 1) + min);
+
+        return randomPosition;
+    }
+
+    for(i = 0; i < 12; i++){
+        const cardHTML = `<div class="card" id="${i}" onclick="showCardColor(this)">CARTA</div>`
+        let selectedCardColor = null;
+        selectedCardColor = cardColorOptions.splice(selectCardColor(cardColorOptions), 1)[0];
+
+        cardList[i] = cardHTML;
+        cardColor[i] = selectedCardColor;
+    }
+
+    board.innerHTML = cardList.toString().replaceAll(',', '');
+    for(let card in cardList){
+        cardList[card] = document.getElementById(i);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', createGame());
+
+let currentCards = [];
+
+function addCard(card){
+    if(currentCards.length !== 0){
+        currentCards[1] = card;
+
+        if(currentCards[0].style.backgroundColor !== currentCards[1].style.backgroundColor){
+            setTimeout(() => {turnCardsBack(currentCards); currentCards = [];}, 1000);
+        }else{
+            currentCards = [];
+        }
+    }else if(currentCards.length === 0){
+        currentCards[0] = card;
+    }
+}
+
+function showCardColor(card){
+    if(!currentCards.length || currentCards.length === 1){
+        card.style.backgroundColor = cardColor[card.id];
+        addCard(card);
+    }
+}
+
+
+function turnCardsBack(currentCards){
+    for(let card in currentCards){
+        console.log(card);
+        currentCards[card].style.backgroundColor = 'grey';
+    }
+}
